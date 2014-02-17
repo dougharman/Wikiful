8a238868fdf40e6250cdfceb47eaf4946371ec23
giteup <dharman@4harman.com>
2014-02-17 19:49:19 +0000

    modified category.rb, removing validation of content and categories so that I could load the database with rake db:seed

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,4 @@ class Category < ActiveRecord::Base
   has_many :article_categories
   has_many :articles, through: :article_categories
   validates :name, presence: true
-  validates :content, presence: true
-  validates :articles, presence: true
 end
